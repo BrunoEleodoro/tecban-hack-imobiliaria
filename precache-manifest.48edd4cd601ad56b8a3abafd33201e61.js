d92fcf15c3e1515e748312407a122199956e8470
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-07-26 21:04:41 -0300

    Updates

---
+++ precache-manifest.48edd4cd601ad56b8a3abafd33201e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e335751f31b8f72a57032230185e9bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "774509919d8614e0b1a8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f9e5d180f4f1539fd77",
+    "url": "/static/js/2.cea7fce9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cea7fce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774509919d8614e0b1a8",
+    "url": "/static/js/main.83f5c97f.chunk.js"
+  },
+  {
+    "revision": "d9ac6eda04eddefbb7fa",
+    "url": "/static/js/runtime-main.5295aa5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
